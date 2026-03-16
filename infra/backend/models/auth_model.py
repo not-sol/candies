14694691b1bd07a9a96454d1558aaa0cd17a69e4
not-sol <castones.sol@gmail.com>
2026-03-16 14:00:27 +0800

    auth models

--- infra/backend/models/auth_model.py
+++ infra/backend/models/auth_model.py
@@ -6,6 +6,26 @@ class UserLogin(BaseModel):
     password: str
 
 
+class UserRegister(BaseModel):
+    email: EmailStr
+    username: str
+    password: str
+
+
+class RefreshTokenRequest(BaseModel):
+    refresh_token: str
+
+
+class ForgotPasswordRequest(BaseModel):
+    email: EmailStr
+
+
+class ResetPasswordRequest(BaseModel):
+    email: EmailStr
+    confirmation_code: str
+    new_password: str
+
+
 class TokenResponse(BaseModel):
     access_token: str
     id_token: str
